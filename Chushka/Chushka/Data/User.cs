2adcdd79e9d9179786731350d4379820187466d1
vangel hristov <vangel.hristov@yahoo.com>
2018-11-13 17:07:20 +0200

    initial commit

---
+++ Chushka/Chushka/Data/User.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Chushka.Data
+{
+    public enum Role
+    {
+        User,
+        Admin
+    }
+
+    public class User
+    {
+        [Key, Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public Role Role { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
